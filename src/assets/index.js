af6ca18638220b5c5205d023417ad68d4f9bbdd4
aarsh21 <aarshpersonal21@gmail.com>
2023-05-18 17:30:42 +0530

    added changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,13 @@ import loader from './loader.svg'
 import copy from './copy.svg'
 import logo from './logo.svg'
 import tick from './tick.svg'
+import enzyme from './enzyme.png'
 
 export {
     linkIcon,
     loader,
     copy,
     logo,
-    tick
+    tick,
+    enzyme
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { logo } from "../assets";
+import { enzyme } from "../assets";
 
 const Hero = () => {
   return (
@@ -11,11 +11,11 @@ const Hero = () => {
         className="flex justify-between items-center
           w-full mb-10 pt-3 sticky"
       >
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img src={enzyme} alt="logo" className="w-10  pt-1 object-contain" />
         <button
           type="button"
           onClick={() => {
-            window.open("https://github.com/aarsh21/Twitter-Clone");
+            window.open("https://github.com/aarsh21/Ai_Summarizer");
           }}
           className="black_btn"
         >
@@ -23,17 +23,17 @@ const Hero = () => {
         </button>
       </nav>
       <h1 className="head_text">
-        Summarize Article with <br className=" max-md:hidden" />
+        Digest Article With <br className=" max-md:hidden" />
         <span className="orange_gradient">Open Ai GPT-4</span>
       </h1>
       <h2 className="desc">
-        A text summarizer is a clever tool that quickly condenses large texts
+        A text Digester is a clever tool that quickly condenses large texts
         into concise summaries. It harnesses the power of AI to analyze and
         extract the most important information, giving you the essence of the
         content in a nutshell. Whether it's news articles, research papers, or
         online content, this time-saving tool helps you grasp the main ideas
         effortlessly, making information retrieval a breeze. Say goodbye to
-        lengthy reading and hello to quick insights with a text summarizer!
+        lengthy reading and hello to quick insights with a text Digester!
       </h2>
     </header>
   );
